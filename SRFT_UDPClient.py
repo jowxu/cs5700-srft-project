0ee33e3ccbda846644d1bb4d4730eccc355f1004
jxujow <jxujow@gmail.com>
2026-04-21 01:42:26 -0400

    feat: added client report and slightly reformated server report to match sample report provided by TA

--- SRFT_UDPClient.py
+++ SRFT_UDPClient.py
@@ -3,6 +3,8 @@ import sys
 import secrets
 import hmac
 import hashlib
+import time
+import os
 from SRFT_Config import SERVER_IP, SERVER_PORT, CLIENT_IP, CLIENT_PORT, PSK
 from SRFT_Utils import TYPE_DATA, TYPE_ACK, TYPE_REQ, TYPE_FIN, build_packet, parse_packet, parse_server_hello, calc_file_digest_path, confirm_checksum
 from Security import decrypt_payload
@@ -21,6 +23,22 @@ class SRFT_UDPClient:
         self.server_ip = SERVER_IP
         self.server_port = SERVER_PORT
 
+        #stats for client transfer report
+        self.stats = {
+            "security_enabled": "Yes",
+            "handshake_status": "Fail",
+            "filename": "",
+            "filesize": 0,
+            "packets_received": 0,
+            "duplicate_packets": 0,
+            "out_of_order_packets": 0,
+            "checksum_errors": 0,
+            "aead_auth_failures": 0,
+            "received_file_md5": "",
+            "sha256_match": "No",
+            "start_time": 0
+        }
+        
         try:
             # raw send socket, headers created manually
             self.send_sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
@@ -86,6 +104,7 @@ class SRFT_UDPClient:
 
                 # confirm checksum for corruption
                 if not confirm_checksum(p_type, checksum, seq, ack, payload):
+                    self.stats["checksum_errors"] += 1
                     print(f"corrupted packet discarded: seq={seq}")
                     continue
 
@@ -97,6 +116,7 @@ class SRFT_UDPClient:
                     try:
                         payload = decrypt_payload(enc_key, session_id, seq, ack, p_type, payload)
                     except InvalidTag:
+                        self.stats["aead_auth_failures"] += 1
                         print(f"AEAD authentication failed — dropping packet seq={seq}")
                         if p_type == TYPE_DATA:
                             received_seqs.discard(seq)
@@ -115,6 +135,7 @@ class SRFT_UDPClient:
  
                 # Duplicate detection 
                 if seq in received_seqs:
+                    self.stats["duplicate_packets"] += 1
                     print(f"duplicate packet discarded: seq={seq}")
                     self.send_cumulative_ack(expected_seq - 1) 
                     continue
@@ -122,8 +143,13 @@ class SRFT_UDPClient:
                 # mark this sequence number as seen
                 received_seqs.add(seq)
 
+                # record out of order packets
+                if seq != expected_seq:
+                    self.stats["out_of_order_packets"] += 1
+
                 # store in buffer keyed by sequence number
                 recv_buffer[seq] = payload
+                self.stats["packets_received"] += 1
                 print(f"received packet seq={seq}")
  
                 # write any contiguous sequence of packets starting from expected_seq
@@ -138,27 +164,37 @@ class SRFT_UDPClient:
                     self.send_cumulative_ack(expected_seq - 1)
                     ack_counter = 0
                 
+        # record file size and md5sum
+        self.stats["filesize"] = os.path.getsize(output_filename)
+        with open(output_filename, "rb") as f:
+            self.stats["received_file_md5"] = hashlib.md5(f.read()).hexdigest()
+
         # calculate local file digest
         local_digest = calc_file_digest_path(output_filename)
+
         # compare digests
         print(f"received digest: {received_digest}")
         print(f"local digest: {local_digest}")
         if local_digest == received_digest:
+            self.stats["sha256_match"] = "Yes"
             print("digest match: file integrity verified")
         else:
+            self.stats["sha256_match"] = "No"
             print("digest mismatch: file integrity compromised")
 
         print(f"file saved as '{output_filename}'")
 
     def run(self, filename, enc_key=None, session_id=None):
         """
-         Full client workflow:
-            1. Send client hello
-            2. Receive server hello & derive session key
-            3. Send file request to server
-            4. Receive and reassemble the file
+        Full client workflow:
+        1. Send client hello
+        2. Receive server hello & derive session key
+        3. Send file request to server
+        4. Receive and reassemble the file
         """
         output_filename = f"received_{filename}"
+        self.stats["filename"] = filename
+        self.stats["start_time"] = time.time()
 
         # construct client hello
         nonce_client = secrets.token_bytes(16)
@@ -186,7 +222,9 @@ class SRFT_UDPClient:
         hmac_calc = hmac.digest(PSK, server_msg, hashlib.sha256)
         verified = hmac.compare_digest(hmac_server, hmac_calc)
         if (verified is False):
+            self.generate_output_report()
             return 1
+        self.stats["handshake_status"] = "Success"
         
         hkdf_input = PSK + nonce_client + nonce_server + server_session_id
         # derive session key with HKDF put in enc_key
@@ -195,7 +233,36 @@ class SRFT_UDPClient:
         
         self.request_file(filename)
         self.receive_file(output_filename, enc_key=enc_key, session_id=server_session_id)
+        self.generate_output_report()
         return 0
+    
+    def generate_output_report(self):
+        # Calculate the transfer duration
+        duration_seconds = int(time.time() - self.stats["start_time"])
+        duration_formatted = time.strftime('%H:%M:%S', time.gmtime(duration_seconds))
+
+        # Define the output filename
+        base_name = self.stats['filename'].rsplit('.', 1)[0]
+        report_name = f"client_report_{base_name}.txt"
+
+        with open(report_name, 'w') as f:
+            f.write("====================================================\n")
+            f.write("CLIENT REPORT\n")
+            f.write("====================================================\n")
+            f.write(f"Security enabled (PSK + AEAD): {self.stats['security_enabled']}\n")
+            f.write(f"Handshake status: {self.stats['handshake_status']}\n")
+            f.write(f"Size of the transferred file: {self.stats['filesize']} bytes\n")
+            f.write(f"Number of packets received from the server: {self.stats['packets_received']}\n")
+            f.write(f"Number of duplicate packets: {self.stats['duplicate_packets']}\n")
+            f.write(f"Number of out-of-order packets: {self.stats['out_of_order_packets']}\n")
+            f.write(f"Number of packets with checksum errors: {self.stats['checksum_errors']}\n")
+            f.write(f"Time duration of the file transfer: {duration_formatted}\n")
+            f.write(f"Received file MD5: {self.stats['received_file_md5']}\n")
+            f.write(f"AEAD authentication failures: {self.stats['aead_auth_failures']}\n")
+            f.write(f"SHA-256 match: {self.stats['sha256_match']}\n")
+            f.write("====================================================\n")
+        
+        print(f"Client summary report generated: {report_name}")
 
 
 if __name__ == "__main__":
